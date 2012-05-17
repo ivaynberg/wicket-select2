93265d39f1c5649dc7a93f50cf5aed3b7633538f
Igor Vaynberg <igor.vaynberg@gmail.com>
2012-05-16 18:26:40 -0700

    initial checkin

---
+++ wicket-select2/src/main/java/com/vaynberg/wicket/select2/AbstractSelect2Choice.java
@@ -0,0 +1,174 @@
+/*
+ * Copyright 2012 Igor Vaynberg
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this work except in compliance with
+ * the License. You may obtain a copy of the License in the LICENSE file, or at:
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+package com.vaynberg.wicket.select2;
+
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+
+import org.apache.wicket.IResourceListener;
+import org.apache.wicket.markup.html.IHeaderResponse;
+import org.apache.wicket.markup.html.form.HiddenField;
+import org.apache.wicket.model.IModel;
+import org.apache.wicket.request.IRequestParameters;
+import org.apache.wicket.request.Request;
+import org.apache.wicket.request.cycle.RequestCycle;
+import org.apache.wicket.request.http.WebRequest;
+import org.apache.wicket.request.http.WebResponse;
+import org.json.JSONException;
+import org.json.JSONWriter;
+
+/**
+ * Base class for Select2 components
+ * 
+ * @author igor
+ * 
+ * @param <T>
+ *            type of choice object
+ * @param <M>
+ *            type of model object
+ */
+abstract class AbstractSelect2Choice<T, M> extends HiddenField<M> implements IResourceListener {
+
+    private final Settings settings = new Settings();
+
+    private final ChoiceProvider<T> provider;
+
+    /**
+     * Constructor
+     * 
+     * @param id
+     *            component id
+     * @param model
+     *            component model
+     * @param provider
+     *            choice provider
+     */
+    public AbstractSelect2Choice(String id, IModel<M> model, ChoiceProvider<T> provider) {
+	super(id, model);
+	this.provider = provider;
+
+	add(new Select2ResourcesBehavior());
+
+	setOutputMarkupId(true);
+    }
+
+    /**
+     * @return Select2 settings for this component
+     */
+    public final Settings getSettings() {
+	return settings;
+    }
+
+    /**
+     * @return choice provider
+     */
+    public ChoiceProvider<T> getProvider() {
+	return provider;
+    }
+
+    @Override
+    public void renderHead(IHeaderResponse response) {
+	super.renderHead(response);
+
+	// initialize select2
+
+	response.renderOnDomReadyJavaScript(String.format("$('#%s').select2(%s);", getMarkupId(), settings.toJson()));
+
+	// select current value
+
+	renderInitializationScript(response);
+    }
+
+    /**
+     * Renders script used to initialize the value of Select2 after it is created so it matches the current model
+     * object.
+     * 
+     * @param response
+     *            header response
+     */
+    protected abstract void renderInitializationScript(IHeaderResponse response);
+
+    @Override
+    protected void onInitialize() {
+	super.onInitialize();
+
+	// configure the ajax callbacks
+
+	AjaxSettings ajax = settings.getAjax(true);
+
+	ajax.setUrl(urlFor(IResourceListener.INTERFACE, null));
+
+	ajax.setData(String
+		.format("function(term, page) { return { term: term, page:page, '%s':true, '%s':[window.location.protocol, '//', window.location.host, window.location.pathname].join('')}; }",
+			WebRequest.PARAM_AJAX, WebRequest.PARAM_AJAX_BASE_URL));
+
+	ajax.setResults("function(data, page) { return data; }");
+    }
+
+    @Override
+    public void onResourceRequested() {
+
+	// this is the callback that retrieves matching choices used to populate the dropdown
+
+	RequestCycle rc = RequestCycle.get();
+	Request request = rc.getRequest();
+	IRequestParameters params = request.getRequestParameters();
+
+	// retrieve choices matching the search term
+
+	String term = params.getParameterValue("term").toOptionalString();
+
+	int page = params.getParameterValue("page").toInt(1);
+	// select2 uses 1-based paging, but in wicket world we are used to
+	// 0-based
+	page -= 1;
+
+	Response<T> response = new Response<T>();
+	provider.query(term, page, response);
+
+	// jsonize and write out the choices to the response
+
+	WebResponse webResponse = (WebResponse) rc.getResponse();
+	webResponse.setContentType("application/json");
+
+	OutputStreamWriter out = new OutputStreamWriter(webResponse.getOutputStream());
+	JSONWriter json = new JSONWriter(out);
+
+	try {
+	    json.object();
+	    json.key("results").array();
+	    for (T item : response) {
+		json.object();
+		provider.toJson(item, json);
+		json.endObject();
+	    }
+	    json.endArray();
+	    json.key("more").value(response.getHasMore()).endObject();
+	} catch (JSONException e) {
+	    throw new RuntimeException("Could not write Json response", e);
+	}
+
+	try {
+	    out.flush();
+	} catch (IOException e) {
+	    throw new RuntimeException("Could not write Json to servlet response", e);
+	}
+    }
+
+    @Override
+    protected void onDetach() {
+	provider.detach();
+	super.onDetach();
+    }
+
+}
---
+++ wicket-select2/src/main/java/com/vaynberg/wicket/select2/Settings.java
@@ -0,0 +1,223 @@
+/*
+ * Copyright 2012 Igor Vaynberg
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this work except in compliance with
+ * the License. You may obtain a copy of the License in the LICENSE file, or at:
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+package com.vaynberg.wicket.select2;
+
+import java.io.Serializable;
+
+import org.json.JSONException;
+import org.json.JSONStringer;
+
+import com.vaynberg.wicket.select2.json.Json;
+
+/**
+ * Select2 settings. Refer to the Select2 documentation for what these options mean.
+ * 
+ * @author igor
+ */
+public final class Settings implements Serializable {
+
+    private Integer minimumInputLength;
+    private Integer minimumResultsForSearch;
+    private Object placeholder;
+    private Boolean allowClear;
+    private Boolean multiple;
+    private Boolean closeOnSelect;
+    private String id;
+    private String formatSelection;
+    private String formatResult;
+    private String formatNoMatches;
+    private String formatInputTooShort;
+    private String createSearchChoice;
+    private String initSelection;
+    private String query;
+
+    private AjaxSettings ajax;
+    private String data;
+    private String tags;
+
+    public CharSequence toJson() {
+	try {
+	    JSONStringer writer = new JSONStringer();
+	    writer.object();
+	    Json.writeObject(writer, "minimumInputLength", minimumInputLength);
+	    Json.writeObject(writer, "minimumResultsForSearch", minimumResultsForSearch);
+	    Json.writeObject(writer, "placeholder", placeholder);
+	    Json.writeObject(writer, "allowClear", allowClear);
+	    Json.writeObject(writer, "multiple", multiple);
+	    Json.writeObject(writer, "closeOnSelect", closeOnSelect);
+	    Json.writeFunction(writer, "id", id);
+	    Json.writeFunction(writer, "formatSelection", formatSelection);
+	    Json.writeFunction(writer, "formatResult", formatResult);
+	    Json.writeFunction(writer, "formatNoMatches", formatNoMatches);
+	    Json.writeFunction(writer, "formatInputTooShort", formatInputTooShort);
+	    Json.writeFunction(writer, "createSearchChoice", createSearchChoice);
+	    Json.writeFunction(writer, "initSelection", initSelection);
+	    Json.writeFunction(writer, "query", query);
+
+	    writer.key("ajax");
+	    ajax.toJson(writer);
+
+	    Json.writeFunction(writer, "data", data);
+	    Json.writeFunction(writer, "tags", tags);
+	    writer.endObject();
+
+	    return writer.toString();
+	} catch (JSONException e) {
+	    throw new RuntimeException("Could not convert Select2 settings object to Json", e);
+	}
+    }
+
+    public Integer getMinimumInputLength() {
+	return minimumInputLength;
+    }
+
+    public void setMinimumInputLength(Integer minimumInputLength) {
+	this.minimumInputLength = minimumInputLength;
+    }
+
+    public Integer getMinimumResultsForSearch() {
+	return minimumResultsForSearch;
+    }
+
+    public void setMinimumResultsForSearch(Integer minimumResultsForSearch) {
+	this.minimumResultsForSearch = minimumResultsForSearch;
+    }
+
+    public Object getPlaceholder() {
+	return placeholder;
+    }
+
+    public void setPlaceholder(Object placeholder) {
+	this.placeholder = placeholder;
+    }
+
+    public Boolean getAllowClear() {
+	return allowClear;
+    }
+
+    public void setAllowClear(Boolean allowClear) {
+	this.allowClear = allowClear;
+    }
+
+    public Boolean getMultiple() {
+	return multiple;
+    }
+
+    public void setMultiple(Boolean multiple) {
+	this.multiple = multiple;
+    }
+
+    public Boolean getCloseOnSelect() {
+	return closeOnSelect;
+    }
+
+    public void setCloseOnSelect(Boolean closeOnSelect) {
+	this.closeOnSelect = closeOnSelect;
+    }
+
+    public String getId() {
+	return id;
+    }
+
+    public void setId(String id) {
+	this.id = id;
+    }
+
+    public String getFormatSelection() {
+	return formatSelection;
+    }
+
+    public void setFormatSelection(String formatSelection) {
+	this.formatSelection = formatSelection;
+    }
+
+    public String getFormatResult() {
+	return formatResult;
+    }
+
+    public void setFormatResult(String formatResult) {
+	this.formatResult = formatResult;
+    }
+
+    public String getFormatNoMatches() {
+	return formatNoMatches;
+    }
+
+    public void setFormatNoMatches(String formatNoMatches) {
+	this.formatNoMatches = formatNoMatches;
+    }
+
+    public String getFormatInputTooShort() {
+	return formatInputTooShort;
+    }
+
+    public void setFormatInputTooShort(String formatInputTooShort) {
+	this.formatInputTooShort = formatInputTooShort;
+    }
+
+    public String getCreateSearchChoice() {
+	return createSearchChoice;
+    }
+
+    public void setCreateSearchChoice(String createSearchChoice) {
+	this.createSearchChoice = createSearchChoice;
+    }
+
+    public String getInitSelection() {
+	return initSelection;
+    }
+
+    public void setInitSelection(String initSelection) {
+	this.initSelection = initSelection;
+    }
+
+    public String getQuery() {
+	return query;
+    }
+
+    public void setQuery(String query) {
+	this.query = query;
+    }
+
+    public AjaxSettings getAjax() {
+	return getAjax(false);
+    }
+
+    public AjaxSettings getAjax(boolean createIfNotSet) {
+	if (createIfNotSet) {
+	    ajax = new AjaxSettings();
+	}
+	return ajax;
+    }
+
+    public void setAjax(AjaxSettings ajax) {
+	this.ajax = ajax;
+    }
+
+    public String getData() {
+	return data;
+    }
+
+    public void setData(String data) {
+	this.data = data;
+    }
+
+    public String getTags() {
+	return tags;
+    }
+
+    public void setTags(String tags) {
+	this.tags = tags;
+    }
+
+}
