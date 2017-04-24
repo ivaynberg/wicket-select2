e81827f018f0ce4b75afab7dcb86635c57662753
Justin Graham <Justin.af.graham@gmail.com>
2017-04-24 06:57:14 -0700

    Added grouped multi select (#111)

--- wicket-select2/src/main/java/com/vaynberg/wicket/select2/AbstractSelect2Choice.java
+++ wicket-select2/src/main/java/com/vaynberg/wicket/select2/AbstractSelect2Choice.java
@@ -16,7 +16,6 @@ import java.io.IOException;
 import java.io.OutputStreamWriter;
 
 import org.apache.wicket.IResourceListener;
-import org.apache.wicket.behavior.Behavior;
 import org.apache.wicket.model.IModel;
 import org.apache.wicket.request.IRequestParameters;
 import org.apache.wicket.request.Request;
@@ -26,13 +25,10 @@ import org.json.JSONWriter;
 
 /**
  * Base class for Select2 components
- * 
+ *
+ * @param <T> type of choice object
+ * @param <M> type of model object
  * @author igor
- * 
- * @param <T>
- *            type of choice object
- * @param <M>
- *            type of model object
  */
 abstract class AbstractSelect2Choice<T, M> extends Select2ChoiceBaseComponent<M> implements IResourceListener {
 
@@ -40,143 +36,134 @@ abstract class AbstractSelect2Choice<T, M> extends Select2ChoiceBaseComponent<M>
 
     /**
      * Constructor
-     * 
-     * @param id
-     *            component id
+     *
+     * @param id component id
      */
     public AbstractSelect2Choice(String id) {
-	this(id, null, null);
+        this(id, null, null);
     }
 
     /**
      * Constructor
-     * 
-     * @param id
-     *            component id
-     * @param model
-     *            component model
+     *
+     * @param id    component id
+     * @param model component model
      */
     public AbstractSelect2Choice(String id, IModel<M> model) {
-	this(id, model, null);
+        this(id, model, null);
     }
 
     /**
      * Constructor.
-     * 
-     * @param id
-     *            component id
-     * @param provider
-     *            choice provider
+     *
+     * @param id       component id
+     * @param provider choice provider
      */
     public AbstractSelect2Choice(String id, ChoiceProvider<T> provider) {
-	this(id, null, provider);
+        this(id, null, provider);
     }
 
     /**
      * Constructor
-     * 
-     * @param id
-     *            component id
-     * @param model
-     *            component model
-     * @param provider
-     *            choice provider
+     *
+     * @param id       component id
+     * @param model    component model
+     * @param provider choice provider
      */
     public AbstractSelect2Choice(String id, IModel<M> model, ChoiceProvider<T> provider) {
-	super(id, model);
-	this.provider = provider;
-	
+        super(id, model);
+        this.provider = provider;
     }
 
-   
 
     /**
      * Sets the choice provider
-     * 
+     *
      * @param provider
      */
     public final void setProvider(ChoiceProvider<T> provider) {
-	this.provider = provider;
+        this.provider = provider;
     }
 
     /**
      * @return choice provider
      */
     public final ChoiceProvider<T> getProvider() {
-	if (provider == null) {
-	    throw new IllegalStateException("Select2 choice component: " + getId()
-		    + " does not have a ChoiceProvider set");
-	}
-	return provider;
+        if (provider == null) {
+            throw new IllegalStateException("Select2 choice component: " + getId()
+                    + " does not have a ChoiceProvider set");
+        }
+        return provider;
     }
 
     @Override
     protected void onConfigure() {
-	super.onConfigure();
+        super.onConfigure();
 
-	getSettings().getAjax().setUrl(urlFor(IResourceListener.INTERFACE, null));
+        getSettings().getAjax().setUrl(urlFor(IResourceListener.INTERFACE, null));
     }
 
-   
 
     @Override
     public void onResourceRequested() {
 
-	// this is the callback that retrieves matching choices used to populate the dropdown
+        // this is the callback that retrieves matching choices used to populate the dropdown
 
-	Request request = getRequestCycle().getRequest();
-	IRequestParameters params = request.getRequestParameters();
+        Request request = getRequestCycle().getRequest();
+        IRequestParameters params = request.getRequestParameters();
 
-	// retrieve choices matching the search term
+        // retrieve choices matching the search term
 
-	String term = params.getParameterValue("term").toOptionalString();
+        String term = params.getParameterValue("term").toOptionalString();
 
-	int page = params.getParameterValue("page").toInt(1);
-	// select2 uses 1-based paging, but in wicket world we are used to
-	// 0-based
-	page -= 1;
+        int page = params.getParameterValue("page").toInt(1);
+        // select2 uses 1-based paging, but in wicket world we are used to
+        // 0-based
+        page -= 1;
 
-	Response<T> response = new Response<T>();
-	provider.query(term, page, response);
+        Response<T> response = new Response<T>();
+        getProvider().query(term, page, response);
 
-	// jsonize and write out the choices to the response
+        // jsonize and write out the choices to the response
 
-	WebResponse webResponse = (WebResponse) getRequestCycle().getResponse();
-	webResponse.setContentType("application/json");
+        WebResponse webResponse = (WebResponse) getRequestCycle().getResponse();
+        webResponse.setContentType("application/json");
 
-	OutputStreamWriter out = new OutputStreamWriter(webResponse.getOutputStream(), getRequest().getCharset());
-	JSONWriter json = new JSONWriter(out);
+        OutputStreamWriter out = new OutputStreamWriter(webResponse.getOutputStream(), getRequest().getCharset());
+        JSONWriter json = new JSONWriter(out);
 
-	try {
-	    json.object();
-	    json.key("results").array();
-	    for (T item : response) {
-		json.object();
-		provider.toJson(item, json);
-		json.endObject();
-	    }
-	    json.endArray();
-	    json.key("more").value(response.getHasMore()).endObject();
-	} catch (JSONException e) {
-	    throw new RuntimeException("Could not write Json response", e);
-	}
+        try {
+            json.object().key("results").array();
+            addValues(json, response);
+            json.endArray().key("more").value(response.getHasMore()).endObject();
+        } catch (JSONException e) {
+            throw new RuntimeException("Could not write Json response", e);
+        }
 
-	try {
-	    out.flush();
-	} catch (IOException e) {
-	    throw new RuntimeException("Could not write Json to servlet response", e);
-	}
+        try {
+            out.flush();
+        } catch (IOException e) {
+            throw new RuntimeException("Could not write Json to servlet response", e);
+        }
     }
 
     @Override
     protected void onDetach() {
-    	provider.detach();
-    	super.onDetach();
+        getProvider().detach();
+        super.onDetach();
     }
 
     @Override
     protected boolean getStatelessHint() {
         return false;
     }
-    
+
+
+    protected void addValues(final JSONWriter json, final Iterable<T> response) throws JSONException {
+        for (T item : response) {
+            json.object();
+            getProvider().toJson(item, json);
+            json.endObject();
+        }
+    }
 }
---
+++ wicket-select2/src/main/java/com/vaynberg/wicket/select2/Select2GroupedMultiChoice.java
@@ -0,0 +1,107 @@
+package com.vaynberg.wicket.select2;
+
+import org.apache.wicket.model.IModel;
+import org.json.JSONException;
+import org.json.JSONWriter;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author Justin Graham
+ * @since 4/19/17
+ */
+public class Select2GroupedMultiChoice<T extends GroupedValue> extends Select2MultiChoice<T> {
+
+    /** The key to group values which have null or empty groups */
+    private static final String UNGROUPED = "UNGROUPED";
+
+    public Select2GroupedMultiChoice(String id, IModel<Collection<T>> model, ChoiceProvider<T> provider) {
+        super(id, model, provider);
+    }
+
+    public Select2GroupedMultiChoice(String id, IModel<Collection<T>> model) {
+        super(id, model);
+    }
+
+    public Select2GroupedMultiChoice(String id) {
+        super(id);
+    }
+
+    /**
+     * Handles building the grouped JSON structure to provide to Select2
+     *
+     * <pre>{@code
+     * {
+     *   "results": [
+     *     {
+     *       "text": "group1",
+     *       "children": [
+     *         {
+     *           "id": 1,
+     *           "text": "Some Text"
+     *         },
+     *         {
+     *           "id": 2,
+     *           "text": "Some More Text"
+     *         }
+     *       ]
+     *     },
+     *     {
+     *       "text": "group2",
+     *       "children": [
+     *         {
+     *           "id": 3,
+     *           "text": "Another Item"
+     *         }
+     *       ]
+     *     }
+     *   ],
+     *   "more": true
+     * }
+     * }</pre>
+     *
+     * @param json the writer to append the JSON nodes to
+     * @param response Iterable collection of objects extending {@link GroupedValue}
+     * @throws JSONException if the response contains malformed JSON
+     */
+    @Override
+    protected void addValues(final JSONWriter json, final Iterable<T> response) throws JSONException {
+        final Map<String, List<T>> groupedItems = groupItems(response);
+        for (Map.Entry<String, List<T>> entry : groupedItems.entrySet()) {
+            if (UNGROUPED.equals(entry.getKey())) {
+                super.addValues(json, entry.getValue());
+                continue;
+            }
+            json.object().key("text").value(entry.getKey()).key("children").array();
+            super.addValues(json, entry.getValue());
+            json.endArray().endObject();
+        }
+    }
+
+    /**
+     * Groups the {@link Iterable<T>} on the {@link GroupedValue#group}. If the value's group is null or
+     * empty its added as {@link Select2GroupedMultiChoice#UNGROUPED}
+     *
+     * @param response Iterable collection of objects extending {@link GroupedValue}
+     * @return the sorted values
+     */
+    protected Map<String, List<T>> groupItems(final Iterable<T> response) {
+        final Map<String, List<T>> groupedItems = new HashMap<String, List<T>>();
+        for (final T item : response) {
+            final String group = item.getGroup();
+            final String key = (group == null || group.isEmpty()) ? UNGROUPED : group;
+            if (groupedItems.containsKey(key)) {
+                groupedItems.get(key).add(item);
+                continue;
+            }
+            groupedItems.put(key, new ArrayList<T>() {{
+                add(item);
+            }});
+        }
+        return groupedItems;
+    }
+}
